0b66e11957466a1addc3acb676dfae44b81dd6bb
Ayush Gupta <ayushcmf@gmail.com>
2026-01-15 00:55:19 +0530

    cms vercel check 1

--- api/auth.js
+++ api/auth.js
@@ -1,10 +1,12 @@
 import OAuthProvider from "netlify-cms-oauth-provider-node";
 
-const provider = new OAuthProvider({
-  github: {
-    clientId: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  },
-});
+export default function handler(req, res) {
+  const provider = new OAuthProvider({
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+  });
 
-export default provider.handler;
+  return provider.handler(req, res);
+}
